463ad58ea7876dbac03260efb1dd9237989a8466
Cesar Preza <cpreza93@yahoo.com>
2024-12-15 18:42:27 -0800

    adds session authorization

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,8 +8,32 @@ router.get('/sign-up', (req, res) => {
     res.render('auth/sign-up.ejs');
 });
 
-router.get('/sign-up', (req, res) => {
-    res.render('/auth/sign-up.ejs');
+router.get('/sign-in', (req, res) => {
+    res.render('auth/sign-in.ejs');
+});
+
+router.get('/sign-out', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+
+router.post('/sign-in', async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (!userInDatabase) {
+        return res.send('Login failed. Please try again.');
+    }
+    const validPassword = bcrypt.compareSync(
+        req.body.password,
+        userInDatabase.password
+    );
+    if (!validPassword) {
+        return res.send('Login failed. Please try again.');
+    }
+    req.session.user = {
+        username: userInDatabase.username,
+        _id: userInDatabase._id,
+    };
+    res.redirect('/');
 });
 
 router.post('/sign-up', async (req, res) => {
